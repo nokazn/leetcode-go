f7fcebc1c013b24c964db4fbe631afacc014fbe0
nokazn <nokazn@gmail.com>
2020-11-15 20:14:56 +0900

    update twoSum

--- problems/0001.Two-Sum/twoSum.go
+++ problems/0001.Two-Sum/twoSum.go
@@ -1,14 +1,13 @@
 package leetcode
 
 func twoSum(nums []int, target int) []int {
-  indexes := make([]int, 2)
   for i1, v1 := range nums {
     offset := i1 + 1
     for i2, v2 := range nums[offset:] {
       if (v1 + v2 == target) {
-        indexes = []int{ i1, i2 + offset }
+        return []int{ i1, i2 + offset }
       }
     }
   }
-  return indexes
+  return []int { 0, 0 }
 }
