edeba6c79b5ba046ed52b4b752809de3a5212c47
nokazn <nokazn@gmail.com>
2020-11-15 20:26:54 +0900

    chore: modify directory name & fmt

---
+++ problems/00001.Two-Sum/twoSum.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func twoSum(nums []int, target int) []int {
+	for i1, v1 := range nums {
+		offset := i1 + 1
+		for i2, v2 := range nums[offset:] {
+			if v1+v2 == target {
+				return []int{i1, i2 + offset}
+			}
+		}
+	}
+	return []int{0, 0}
+}
