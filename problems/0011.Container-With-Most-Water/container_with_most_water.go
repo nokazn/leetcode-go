285a0cb3175af967b0b563f925f3f17a2a6dd678
nokazn <41154684+nokazn@users.noreply.github.com>
2022-04-17 12:37:37 +0900

    fix: speed up for 0011

--- problems/0011.Container-With-Most-Water/container_with_most_water.go
+++ problems/0011.Container-With-Most-Water/container_with_most_water.go
@@ -7,14 +7,26 @@ func min(x, y int) int {
 	return y
 }
 
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
 func maxArea(height []int) int {
 	volume := 0
-	for start, h1 := range height {
-		for x, h2 := range height[start+1:] {
-			s := (x + 1) * min(h1, h2)
-			if s > volume {
-				volume = s
-			}
+	length := len(height)
+	start := 0
+	end := length - 1
+	for start < end {
+		h1 := height[start]
+		h2 := height[end]
+		volume = max(volume, (end-start)*min(h1, h2))
+		if h1 > h2 {
+			end -= 1
+		} else {
+			start += 1
 		}
 	}
 	return volume
